86bb3b4aaade3bc8447bf4ee64a83ba1f517583c
Roberto <robertodermato@gmail.com>
2019-10-14 18:05:48 -0300

    App Conta tratando as exceções

--- src/AppContaCorrente.java
+++ src/AppContaCorrente.java
@@ -2,11 +2,32 @@ public class AppContaCorrente {
 
     public static void main (String args[]){
 
-        //ContaCorrente conta1 = new ContaCorrente(-10); //Gera o erro de saldo inicial menor que zero
+        try {
+            ContaCorrente conta1 = new ContaCorrente(-10); //Gera o erro de saldo inicial menor que zero
+        } catch(IllegalArgumentException e){
+            System.out.println("O saldo informado deve ser maior que zero");
+        }
+
         ContaCorrente conta2 = new ContaCorrente(10);
-        //conta2.deposito(0); // gera erro de deposito igual a zero
-        //conta2.retirada(0); //gera erro de retirada de zero
-        conta2.retirada(30); //gera erro de retirada maior que o saldo
+
+        try {
+            conta2.deposito(0); // gera erro de deposito igual a zero
+        } catch (IllegalArgumentException e){
+            System.out.println("O depósito tem que ser maior que zero!!");
+        }
+
+        try {
+            conta2.retirada(0); //gera erro de retirada de zero
+        } catch(IllegalArgumentException e){
+            System.out.println("Retirada não pode ser zero ou negativa!!!");
+        }
+
+        try {
+            conta2.retirada(30); //gera erro de retirada maior que o saldo
+        } catch(IllegalArgumentException e){
+            System.out.println("Retirada deve ser menor que o saldo!!");
+        }
+
 
 
 
